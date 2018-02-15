536a7ced946b0d18757575bb0a42149f9a5c87ba
Jonathan Herbert <foohyfooh@gmail.com>
2018-02-14 21:52:12 -0400

    Disambiguate id in MenuManager getCategories method

--- backend/db/MenuManager.js
+++ backend/db/MenuManager.js
@@ -7,7 +7,7 @@ class MenuManager extends DatabaseManager {
   */
   getCategories(){
     return this._query(`
-    SELECT category.name, COUNT(menu_item.category) 
+    SELECT category.id AS id, category.name, COUNT(menu_item.category) 
     FROM category 
     JOIN menu_item ON category.id = menu_item.category
     GROUP BY category.name
