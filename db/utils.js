57e8283df248884e07a8fef427d9e5d0ac742d27
Jonathan Herbert <foohyfooh@gmail.com>
2018-03-22 11:01:32 -0400

    Add Checks to readConfig function
    Ensure that the config.json file is in the folder and that it has the necessary fields for the MySQL database.

--- db/utils.js
+++ db/utils.js
@@ -1,10 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.readConfig = function readConfig(){
-  let conf = fs.readFileSync(__dirname + path.sep + 'config.json', {
-    encoding: 'utf8'
-  });
-  conf = JSON.parse(conf);
+ function readConfig(){
+  let contents;
+  try{
+    contents = fs.readFileSync(__dirname + path.sep + 'config.json', {encoding: 'utf8'});
+  }catch(e){
+    console.error('config.json file is missing from db module.');
+    process.exit(0);
+  }
+
+  let conf;
+  try{
+    conf = JSON.parse(contents);
+  }catch(e){
+    console.error('config.json contains malformed JSON.');
+    process.exit(0);
+  }
+
+  let requiredFields = ['host', 'database', 'user', 'password'];
+  let missingRequired = false;
+  for(let field of requiredFields){
+    if(conf[field] === undefined){
+      console.error(`${field} is a required field in config.json`);
+      missingRequired = true;
+    }
+  }
+  if(missingRequired) process.exit(0);
+  
   return conf;
 }
+
+exports.readConfig = readConfig;
